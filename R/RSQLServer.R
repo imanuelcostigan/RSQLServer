0bab5fbcce04ef505e579ad8d31e74933ffcadca
Imanuel Costigan <i.costigan@me.com>
2014-10-08 15:13:50 +1100

    Updated package doc & roxyenised

--- R/RSQLServer.R
+++ R/RSQLServer.R
@@ -4,14 +4,15 @@ NULL
 #' RSQLServer
 #'
 #' Wrapper around RJDBC DBI applied specifically to SQL Server. You will need the
-#' \href{https://www.java.com/en/download/index.jsp}{Java Runtime Environment (version 6 or higher)}.
+#' \href{https://www.java.com/en/download/index.jsp}{Java Runtime Environment}.
 #'
 #' If you intend to use integrated security (Windows Authentication) to
-#' authenticate your server session, you will need to download \href{http://www.microsoft.com/en-us/download/details.aspx?id=11774}{Microsoft's JDBC Driver for SQL Server package}
-#' and copy the bundled driver file (\code{sqljdbc_auth.dll}) to your java
-#' library path (e.g. \code{C:\Program Files (x86)\Java\jre7\bin\client}). Follow
-#' MSDN's \href{http://msdn.microsoft.com/en-us/library/ms378428(v=sql.110).aspx}{Building the Connection URL}
-#' for further information.
+#' authenticate your server session, you will need to download \href{http://sourceforge.net/projects/jtds/files/}{jTDS}
+#' and copy the native single sign on library (\code{ntlmauth.dll}) to any file
+#' on your system's \code{PATH} (e.g. \code{Sys.getenv("PATH")}). This
+#' functionality seems a little flaky in my testing, but it could just be my
+#' setup. Full installation instructions are available in the \code{README.SSO}
+#' file in the jTDS download bundle.
 #'
 #' @docType package
 #' @name RSQLServer
