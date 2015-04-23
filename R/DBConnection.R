d7a3ff4c4a7ddc985cc9318a8acc1d7349c99ebf
Imanuel Costigan <i.costigan@me.com>
2015-04-24 09:23:18 +1000

    dplyr integration; src interface improvements
    - Bring dplyr integration back (#4). See
    
    https://groups.google.com/forum/#!topic/manipulatr/9QrAEMt-9jc/discussion
    - src_sqlserver interface improved to match dbConnect for
    SQLServerDriver and improved documentation
    - Improve performance of printing tbl objects (#12)

--- R/DBConnection.R
+++ R/DBConnection.R
@@ -20,33 +20,7 @@ setClass("SQLServerConnection", contains = 'JDBCConnection')
 #' @param drv An objected of class \code{\linkS4class{SQLServerDriver}}, or an
 #' existing \code{\linkS4class{SQLServerConnection}}. If a connection,
 #' the connection will be cloned.
-#' @param server the server address or recognised alias thereof.
-#' @param file defaults to using the server details file in
-#' \code{$HOME/sql.yaml}. The server details including \code{type}, \code{port}
-#' and any optional \code{properties} can be sourced from this file. If the
-#' \code{server} name is found in \code{file}, the details therein are used
-#' (and in particular, those provided in other arguments to this function are
-#' ignored). The connection method prefers server details to be provided in
-#' a \code{"sql.yaml"} file rather than provided as arguments to this function.
-#' If you wish to specify the details as parameters, ensure that the \code{file}
-#' argument is missing from the call.
-#' @param type the server type. Must be either \code{"sqlserver"} or
-#' \code{"sybase"}. Defaults to \code{"sqlserver"}.
-#' @param port the TCP/IP default port. This will be coerced to a string. Defaults
-#' to \code{1433} if an empty string.
-#' @param database the name of the database hosted on the \code{server}. If an
-#' empty string, a connection to the default database on \code{server} is
-#' assumed.
-#' @param properties One or more \href{http://jtds.sourceforge.net/faq.html}{optional connection properties.}
-#' in a named list. Note if you intend to set the \code{useNTLMv2} property to \code{'true'}
-#' from the default API value of \code{'false'}, you will need to make a specific
-#' authentication driver available to the SQL Server driver, although this hasn't
-#' worked particularly well in testing. See \code{\link{RSQLServer}} for more
-#' details. Should you wish to use Windows authentication to connect to the server,
-#' I recommend you set the following optional parameters: set \code{useNTLMv2} to
-#' \code{'true'}, \code{domain} to your domain and \code{user} and
-#' \code{password} to your username and password on \code{domain}. jTDS'
-#' SSO functionality is flaky.
+#' @template sqlserver-parameters
 #' @return a \code{\linkS4class{SQLServerConnection}}
 #' @examples
 #' # View sql.yaml file bundled in package
