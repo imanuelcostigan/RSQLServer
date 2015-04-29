0dabbe53f9cfadb9737b4aea3b9fd207fcb5eddf
Imanuel Costigan <i.costigan@me.com>
2015-04-29 20:21:50 +1000

    Implemented sql_select method (#18)

--- R/sql-backends.R
+++ R/sql-backends.R
@@ -1,78 +1,80 @@
-# #' @importFrom dplyr sql_select
-# #' @export
-# sql_select.SQLServerConnection <- function(con, select, from, where = NULL,
-#   group_by = NULL, having = NULL, order_by = NULL, top = NULL,
-#   offset = NULL, fetch = NULL, ...) {
-#
-#   out <- vector("list", 9)
-#   names(out) <- c("select", "from", "where", "group_by", "having", "order_by",
-#     "top", "offset", "fetch")
-#
-#   if (!is.null(top)) {
-#     assertthat::assert_that(is.integer(top), length(top) == 1L)
-#     top <- paste0("TOP ", top)
-#     if (is.null(order_by))
-#     {
-#       # MS best practice: use ORDER BY clause when using TOP clause
-#       # This is the only way to predictably indicate which rows are affected by
-#       # TOP.
-#       # Source: http://msdn.microsoft.com/en-us/library/ms189463.aspx
-#       order_by <- dplyr::build_sql("ORDER BY ",
-#         dplyr::escape(order_by, collapse = ", ", con = con))
-#     }
-#   }
-#   assertthat::assert_that(is.character(select), length(select) > 0L)
-#   out$select <- dplyr::build_sql("SELECT ", top,
-#     dplyr::escape(select, collapse = ", ", con = con))
-#   assertthat::assert_that(is.character(from), length(from) == 1L)
-#   out$from <- dplyr::build_sql("FROM ", from, con = con)
-#
-#   if (length(where) > 0L) {
-#     assertthat::assert_that(is.character(where))
-#     out$where <- dplyr::build_sql("WHERE ",
-#       dplyr::escape(where, collapse = " AND ", con = con))
-#   }
-#
-#   if (!is.null(group_by)) {
-#     assertthat::assert_that(is.character(group_by), length(group_by) > 0L)
-#     out$group_by <- dplyr::build_sql("GROUP BY ",
-#       dplyr::escape(group_by, collapse = ", ", con = con))
-#   }
-#
-#   if (!is.null(having)) {
-#     assertthat::assert_that(is.character(having), length(having) == 1L)
-#     out$having <- dplyr::build_sql("HAVING ",
-#       dplyr::escape(having, collapse = ", ", con = con))
-#   }
-#
-#   if (!is.null(order_by)) {
-#     assertthat::assert_that(is.character(order_by), length(order_by) > 0L)
-#     out$order_by <- dplyr::build_sql("ORDER BY ",
-#       dplyr::escape(order_by, collapse = ", ", con = con))
-#   }
-#
-#   if (!is.null(offset)) {
-#     # OFFSET/FETCH supported for SQL Server 2012 and higher (i.e. 11 and higher)
-#     # when using ORDER BY
-#     # http://stackoverflow.com/questions/187998/row-offset-in-sql-server
-#     # MSSQL versioning: http://support2.microsoft.com/kb/321185
-#     # OFFSET/FETCH: http://msdn.microsoft.com/en-us/library/ms188385(v=sql.110).aspx
-#     assertthat::assert_that(!is.null(order_by), dbGetInfo(con)$db.version >= 11,
-#       is.integer(offset), length(offset) == 1L)
-#     out$offset <- dplyr::build_sql("OFFSET ", offset, con = con)
-#   }
-#
-#   if (!is.null(fetch)) {
-#     # SQL Server 2012 + equivalent of LIMIT is FETCH (used with OFFSET)
-#     # out$offset will be non-NULL if it is set and SQL Server dependency is met.
-#     assertthat::assert_that(!is.null(out$offset), is.integer(fetch),
-#       length(fetch) == 1L)
-#     out$fetch <- dplyr::build_sql("FETCH NEXT ", fetch, " ONLY", con = con)
-#   }
-#   dplyr::escape(unname(compact(out)), collapse = "\n", parens = FALSE,
-#     con = con)
-# }
-#
+#' @importFrom dplyr sql_select
+#' @export
+sql_select.SQLServerConnection <- function(con, select, from, where = NULL,
+  group_by = NULL, having = NULL, order_by = NULL, top = NULL,
+  offset = NULL, fetch = NULL, ...) {
+
+  out <- vector("list", 9)
+  names(out) <- c("select", "from", "where", "group_by", "having", "order_by",
+    "top", "offset", "fetch")
+
+  if (!is.null(top)) {
+    assertthat::assert_that(is.integer(top), length(top) == 1L)
+    top <- paste0("TOP ", top)
+  }
+
+  assertthat::assert_that(is.character(select), length(select) > 0L)
+  out$select <- dplyr::build_sql("SELECT ", top,
+    dplyr::escape(select, collapse = ", ", con = con))
+  assertthat::assert_that(is.character(from), length(from) == 1L)
+  out$from <- dplyr::build_sql("FROM ", from, con = con)
+
+  if (length(where) > 0L) {
+    assertthat::assert_that(is.character(where))
+    out$where <- dplyr::build_sql("WHERE ",
+      dplyr::escape(where, collapse = " AND ", con = con))
+  }
+
+  if (!is.null(group_by)) {
+    assertthat::assert_that(is.character(group_by), length(group_by) > 0L)
+    out$group_by <- dplyr::build_sql("GROUP BY ",
+      dplyr::escape(group_by, collapse = ", ", con = con))
+  }
+
+  if (!is.null(having)) {
+    assertthat::assert_that(is.character(having), length(having) == 1L)
+    out$having <- dplyr::build_sql("HAVING ",
+      dplyr::escape(having, collapse = ", ", con = con))
+  }
+
+  if (is.null(order_by)) {
+    if (!is.null(top)) {
+      # MS best practice: use ORDER BY clause when using TOP clause
+      # This is the only way to predictably indicate which rows are affected by
+      # TOP.
+      # Source: http://msdn.microsoft.com/en-us/library/ms189463.aspx
+      order_by <- dplyr::build_sql("ORDER BY ",
+        dplyr::escape(order_by, collapse = ", ", con = con))
+    }
+  } else {
+    assertthat::assert_that(is.character(order_by), length(order_by) > 0L)
+    out$order_by <- dplyr::build_sql("ORDER BY ",
+      dplyr::escape(order_by, collapse = ", ", con = con))
+  }
+
+  if (!is.null(offset)) {
+    # OFFSET/FETCH supported for SQL Server 2012 and higher (i.e. 11 and higher)
+    # when using ORDER BY
+    # http://stackoverflow.com/questions/187998/row-offset-in-sql-server
+    # MSSQL versioning: http://support2.microsoft.com/kb/321185
+    # OFFSET/FETCH: http://msdn.microsoft.com/en-us/library/ms188385(v=sql.110).aspx
+    assertthat::assert_that(!is.null(order_by), dbGetInfo(con)$db.version >= 11,
+      is.integer(offset), length(offset) == 1L)
+    out$offset <- dplyr::build_sql("OFFSET ", offset, con = con)
+  }
+
+  if (!is.null(fetch)) {
+    # SQL Server 2012 + equivalent of LIMIT is FETCH (used with OFFSET)
+    # out$offset will be non-NULL if it is set and SQL Server dependency is met.
+    assertthat::assert_that(!is.null(out$offset), is.integer(fetch),
+      length(fetch) == 1L)
+    out$fetch <- dplyr::build_sql("FETCH NEXT ", fetch, " ONLY", con = con)
+  }
+
+  dplyr::escape(unname(compact(out)), collapse = "\n", parens = FALSE,
+    con = con)
+}
+
 # #' @importFrom dplyr sql_join
 # #' @export
 # sql_join.SQLServerConnection <- function(con, x, y, type = "inner",
