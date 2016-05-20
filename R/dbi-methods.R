20a1dd854ed7dc23b2d4393b8e4df673557a06f2
Imanuel Costigan <i.costigan@me.com>
2016-05-20 14:13:44 +1000

    Remove .fillStatementParameters
    No longer called by this package

--- R/dbi-methods.R
+++ R/dbi-methods.R
@@ -215,7 +215,7 @@ setMethod("dbExecute", c("SQLServerConnection", "character"),
     rJava::.jcall(stat, "I", "executeUpdate", statement, check = FALSE)
     x <- rJava::.jgetEx(TRUE)
     if (!rJava::is.jnull(x)) {
-      stop("execute JDBC update query failed in dbSendUpdate (",
+      stop("execute JDBC update query failed in dbExecute (",
         rJava::.jcall(x, "S", "getMessage"), ")")
     }
   }
@@ -270,44 +270,6 @@ setMethod("dbDataType", c("SQLServerConnection", "ANY"),
   }
 )
 
-.fillStatementParameters <- function(s, l) {
-  # Modified from RJDBC
-  # https://github.com/s-u/RJDBC/blob/1b7ccd4677ea49a93d909d476acf34330275b9ad/R/class.R#L63
-  for (i in 1:length(l)) {
-    v <- l[[i]]
-    if (is.na(v)) { # map NAs to NULLs (courtesy of Axel Klenk)
-      sqlType <- rToJdbcType(class(v))
-      rJava::.jcall(s, "V", "setNull", i, as.integer(sqlType))
-    } else if (is.integer(v)) {
-      rJava::.jcall(s, "V", "setInt", i, v[1])
-    } else if (is.numeric(v)) {
-      rJava::.jcall(s, "V", "setDouble", i, as.double(v)[1])
-    } else if (is.logical(v)) {
-      rJava::.jcall(s, "V", "setBoolean", i, as.logical(v)[1])
-    } else if (lubridate::is.Date(v)) {
-      # as.POSIXlt sets time to UTC whereas as.POSIXct sets time to local
-      # timezone. The tz argument is ignored when a Date is passed to either
-      # function
-      milliseconds <- as.numeric(as.POSIXlt(v)[1]) * 1000
-      vdate <- rJava::.jnew("java/sql/Date", rJava::.jlong(milliseconds))
-      rJava::.jcall(s, "V", "setDate", i, vdate)
-    } else if (lubridate::is.POSIXct(v)) {
-      # as.integer converts POSIXct to seconds since epoch. Timestamp
-      # constructor needs milliseconds so multiply by 1000
-      # http://docs.oracle.com/javase/7/docs/api/java/sql/Timestamp.html
-      milliseconds <- as.numeric(as.POSIXct(v)[1]) * 1000
-      vtimestamp <- rJava::.jnew("java/sql/Timestamp",
-        rJava::.jlong(milliseconds))
-      rJava::.jcall(s, "V", "setTimestamp", i, vtimestamp)
-    } else if (is.raw(v)) {
-      rJava::.jcall(s, "V", "setByte", i, rJava::.jbyte(as.raw(v)[1]))
-    } else {
-      rJava::.jcall(s, "V", "setString", i, as.character(v)[1])
-    }
-  }
-}
-
-
 #' @rdname SQLServerConnection-class
 #' @export
 setMethod("dbWriteTable", "SQLServerConnection",
@@ -342,14 +304,14 @@ setMethod("dbWriteTable", "SQLServerConnection",
     qname <- ident(name)
     if (!append) {
       ct <- paste("CREATE TABLE ", qname, " (", fdef, ")", sep= '')
-      dbSendUpdate(conn, ct)
+      dbExecute(conn, ct)
     }
     if (length(value[[1]])) {
       # Use Prepared Statement.
       inss <- paste("INSERT INTO ", qname, " VALUES(",
         paste(rep("?", length(value)), collapse = ','), ")", sep = '')
       for (j in 1:length(value[[1]])) {
-        dbSendUpdate(conn, inss, list = as.list(value[j, ]))
+        dbExecute(conn, inss, list = as.list(value[j, ]))
       }
     }
     if (ac) dbCommit(conn)
