ba2a67c516c7f8a91ead164649d0b8f9fe9541d4
Imanuel Costigan <i.costigan@me.com>
2016-05-26 15:02:53 +1000

    Ensure default block is integer

--- R/dbi-methods.R
+++ R/dbi-methods.R
@@ -454,10 +454,9 @@ setMethod("fetch", c("SQLServerResult", "numeric"),
 #' @rdname SQLServerResult-class
 #' @export
 setMethod("dbFetch", c("SQLServerResult", "numeric"),
-  function(res, n, block = 2048, ...) {
+  function(res, n, block = 2048L, ...) {
     # Based on:
     # https://github.com/s-u/RJDBC/blob/1b7ccd4677ea49a93d909d476acf34330275b9ad/R/class.R#L287
-    block <- as.integer(block)
     assertthat::assert_that(assertthat::is.count(block))
     ncols <- rJava::.jcall(res@md, "I", "getColumnCount")
     if (ncols < 1L) return(NULL)
