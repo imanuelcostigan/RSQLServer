22691c4ad70b22c1c5f836d9297eb1b068adebb8
Imanuel Costigan <i.costigan@me.com>
2017-04-10 09:01:18 +1000

    Improve package documentation

--- R/RSQLServer.R
+++ R/RSQLServer.R
@@ -4,11 +4,14 @@ NULL
 #' RSQLServer
 #'
 #' Implements an R Database Interface (DBI) for SQL Server using Microsoft's
-#' JDBC driver. You will need to have at least version 8 of the [Java Runtime
-#' Environment](https://www.java.com/en/download/index.jsp) installed. However,
-#' the necessary JDBC drivers are bundled with the package.
+#' JDBC driver. While the necessary JDBC drivers are bundled with the package,
+#' you will need to have at least version 8 of the [Java Runtime
+#' Environment](https://www.java.com/en/download/index.jsp) installed. Only SQL
+#' Server 2008 and later are supported by the bundled driver.
 #'
 #' @docType package
 #' @name RSQLServer
+#' @references
+#' [System requirements for Microsoft JDBC drivers](https://docs.microsoft.com/en-us/sql/connect/jdbc/system-requirements-for-the-jdbc-driver)
 #' @importFrom purrr %||%
 NULL
