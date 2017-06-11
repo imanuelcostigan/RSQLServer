f8081c323540fe13efe317c66ac2e981627763c8
Imanuel Costigan <i.costigan@me.com>
2017-06-11 19:41:00 +1000

    Check JRE version after loading

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,6 +2,17 @@
 # http://cran.r-project.org/web/packages/helloJavaWorld/vignettes/helloJavaWorld.pdf
 
 .onLoad <- function (libname, pkgname) {
-  rJava::.jpackage(pkgname, lib.loc = libname)
+  res <- rJava::.jpackage(pkgname, lib.loc = libname)
+  # Per "Writing R Extensions"
+  if (java_run_time_version() < 1.8) {
+    stop("Java >= 8 is needed for this package but not available", call. = FALSE)
+  }
+  res
 }
 
+java_run_time_version <- function() {
+  # From "Writing R Extensions"
+  jv <- rJava::.jcall("java/lang/System", "S", "getProperty",
+    "java.runtime.version")
+  numeric_version(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+}
